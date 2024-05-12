cfa01b64e60cf40a390cdbf55ec6d136c0a06b69
SMNoushan <noushan.md@gmail.com>
2024-05-12 22:25:30 +0600

    initialization

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bites Plus
+
+Live Site: [Bites Plus](https://ph-assignment-11-bites-plus-spa.surge.sh/)
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/preline/dist/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "playfair-display": ["Playfair Display", "serif"],
+        "open-sans": ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("preline/plugin")],
+};
