777534b975d84b44442d9a2be233a6f14e1b180e
SMNoushan <noushan.md@gmail.com>
2024-05-13 18:20:17 +0600

    added footer

--- src/components/shared/Spinner.jsx
+++ src/components/shared/Spinner.jsx
@@ -1,6 +1,6 @@
 const Spinner = () => {
   return (
-    <section className="w-full flex justify-center items-center h-[calc(100dvh-112px)]">
+    <section className="w-full flex justify-center items-center h-52 lg:h-[calc(100dvh-488px)]">
       <div
         className="animate-spin inline-block size-16 border-[3px] border-current border-t-transparent text-lime-600 rounded-full"
         role="status"
--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/shared/Header";
+import Footer from "../components/shared/Footer";
 
 const Root = () => {
   return (
     <section className="bg-gray-100">
       <Header />
       <Outlet />
+      <Footer />
     </section>
   );
 };
